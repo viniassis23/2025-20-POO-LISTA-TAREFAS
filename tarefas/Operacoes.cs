a1040b2bd0b6ca254fff5eef029f190656021fa8
Vinicius Assis <viniciusassis2002@gmail.com>
2025-11-21 19:21:31 -0300

    Inserindo metodo Excluir e Alterar

--- tarefas/Operacoes.cs
+++ tarefas/Operacoes.cs
@@ -66,11 +66,40 @@ public class Operacoes
 
     public void Alterar(Tarefa tarefa)
     {
+        using var conexao = new MySqlConnection(connectionString);
+        conexao.Open();
 
+        string sql = @"UPDATE tarefa 
+                       SET nome = @nome, descricao = @descricao, dataCriacao = @dataCriacao, 
+                           status = @status, dataExecucao = @dataExecucao
+                       WHERE id = @id";
+
+        using var cmd = new MySqlCommand(sql, conexao);
+        cmd.Parameters.AddWithValue("@nome", tarefa.Nome);
+        cmd.Parameters.AddWithValue("@descricao", tarefa.Descricao);
+        cmd.Parameters.AddWithValue("@dataCriacao", tarefa.DataCriacao);
+        cmd.Parameters.AddWithValue("@status", tarefa.Status);
+
+        if (tarefa.DataExecucao.HasValue)
+            cmd.Parameters.AddWithValue("@dataExecucao", tarefa.DataExecucao.Value);
+        else
+            cmd.Parameters.AddWithValue("@dataExecucao", DBNull.Value);
+
+        cmd.Parameters.AddWithValue("@id", tarefa.Id);
+
+        cmd.ExecuteNonQuery();
     }
 
     public void Excluir(int id)
     {
+        using var conexao = new MySqlConnection(connectionString);
+        conexao.Open();
 
+        string sql = "DELETE FROM tarefa WHERE id = @id";
+
+        using var cmd = new MySqlCommand(sql, conexao);
+        cmd.Parameters.AddWithValue("@id", id);
+
+        cmd.ExecuteNonQuery();
     }
 }
